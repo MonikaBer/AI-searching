1cbe52eaa43782902af693925ddb694756d8160f
MonikaBer <berlinska.monika@outlook.com>
2019-11-10 13:24:32 +0100

    Creation: - class Individual - class Population - example of input file

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "Individual.hpp"
+#include "Population.hpp"
+#include "lib.hpp"
+
+using namespace std;
+
+
+int main(int argc, char** argv){
+
+
+
+    return 0;
+}
+
 
